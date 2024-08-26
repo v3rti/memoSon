4f86a23cead7b1a35ed07e4e5608f22d60676c57
Amine Bagui <med.bagui.1@gmail.com>
2024-08-26 14:12:38 +0100

    logic but unperfect

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -2,17 +2,7 @@
 
 import { useState, useRef } from 'react';
 
-export default function Card({checked, cardValue, numberClick} : {checked: boolean; cardValue: number; numberClick: Function}){
+export default function Card({cardValue, checked, onClick} : {cardValue: number; checked: boolean; onClick: Function;}){
 
-  const [isChecked, setIsChecked] = useState(false);
-
-  const handleClick = (e) => {
-    setIsChecked(!isChecked);
-    console.log(cardValue);
-  }
-
-  return <div onClick={e => {
-    handleClick(e);
-    numberClick(e);
-  }} className={`${isChecked ? 'bg-[#8C9BA5]' : 'bg-[#fcfcfc]'} w-[100px] h-[100px] rounded-full flex items-center justify-center text-3xl font-medium`}>{isChecked ? cardValue : ""}</div>
+  return <div onClick={onClick} className={`${checked ? 'bg-[#8C9BA5]' : 'bg-[#fcfcfc]'} w-[100px] h-[100px] rounded-full flex items-center justify-center text-3xl font-medium`}>{checked ? cardValue : ""}</div>
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,69 @@
 'use client'
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Card from "./components/Card";
 
 export default function Home() {
 
-  const numbers = [1,2,3,4,5,6,7,8,9,10];
+  const [reveal, setReveal] = useState(0);
+  const [cards, setCards] = useState([{num: 0, checked: false, revealed: false}]);
+  const [nums, setNums] = useState([]);
+
+  const numbers = [1,2,3,5,6,7,8];
   let newNumbers = [...numbers, ...numbers].sort();
 
-  for(let i = 0; i < newNumbers.length; i++){
-    let x = Math.floor(Math.random() * newNumbers.length);
-    let temp = newNumbers[i];
-    newNumbers[i] = newNumbers[x];
-    newNumbers[x] = temp;
-  }
+  useEffect(() => {
+    for(let i = 0; i < newNumbers.length; i++){
+      let x = Math.floor(Math.random() * newNumbers.length);
+      let temp = newNumbers[i];
+      newNumbers[i] = newNumbers[x];
+      newNumbers[x] = temp;
+    }
 
-  const [cloneClicked, setCloneClicked] = useState(0);
-  const [clickedNumber, setClickedNumber] = useState([0,0]);
-  const [doublesFound, setDoublesFound] = useState(false);
+    setNums(newNumbers);
 
-  const handleNumberClick = (e) => {
-    clickedNumber[0] === 0 ? setClickedNumber([e.target, 0]) : setClickedNumber([clickedNumber[0], e.target]);
-    clickedNumber[0] !== 0 && clickedNumber[0] === clickedNumber[1] ? setDoublesFound(true) : "";
-    console.log(clickedNumber);
-    console.log(doublesFound)
+    setCards(newNumbers.map(num => {
+      return {num: num, checked: false, revealed: false}
+    }));
+
+  }, [])
+
+  useEffect(() => {
+    console.log(nums)
+    console.log(cards);
+  }, [nums])
+
+  const revealCard = (i: number) => {
+
+    const cardsCopy = [...cards];
+    const checkCard = cards[i];
+    checkCard.checked = true;
+    cardsCopy[i] = checkCard;
+    setReveal(reveal + 1);
+    console.log(i)
+    if(reveal < 2){
+      setCards(cardsCopy);
+    }else{
+      const newCards = cardsCopy.map((card, idx) => {
+        if(card.num === cardsCopy[i].num && i !== idx && card.checked === true){
+          card.revealed = true;
+          cardsCopy[i].revealed = true;
+        }
+        card.revealed ? card.checked = true : card.checked = false;
+        return card;
+      })
+      setCards(newCards)
+      console.log(newCards)
+      setReveal(0);
+    }
   }
 
   return (
    <div className="flex p-16 flex-col gap-6 h-screen items-center justify-center">
       <div className="flex gap-4 w-[45%] flex-wrap" >
-        {newNumbers.map((num, i) => <Card key={i} cardValue={num} numberClick={(e) => handleNumberClick(e)}/>)}
+        {cards && cards.map((card, i) => <Card onClick={() => {
+          revealCard(i);
+        }} key={i} checked={card.checked} cardValue={card.num} />)}
       </div>
 
    </div>
